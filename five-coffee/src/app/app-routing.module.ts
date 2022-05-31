98bfb9bfc8be622420ee7fa44ee5ad158872172e
dungduy02 <18130048@st.hcmuaf.edu.vn>
2022-05-31 19:44:59 +0700

    home

--- five-coffee/src/app/app-routing.module.ts
+++ five-coffee/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { BodyComponent } from './five-coffee/component/admin/body/body.component';
+import { BodyComponent } from './five-coffee/component/home/body/body.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: BodyComponent},
+  // {path: 'admin', component: BodyComponentHome}
+  // {path: 'login', component: LoginComponent},
+  // {path: 'register', component: RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- five-coffee/src/app/app.module.ts
+++ five-coffee/src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA}  from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FiveCoffeeComponent } from './five-coffee/five-coffee.component';
-import { HeaderComponent } from './five-coffee/component/admin/header/header.component';
-import { BodyComponent } from './five-coffee/component/admin/body/body.component';
+// import { HeaderComponent } from './five-coffee/component/admin/header/header.component';
+// import { BodyComponent } from './five-coffee/component/admin/body/body.component';
+import { HeaderComponent } from './five-coffee/component/home/header/header.component';
+import { BodyComponent } from './five-coffee/component/home/body/body.component';
 import { LoginComponent } from './five-coffee/component/form/login/login.component';
 import { RegisterComponent } from './five-coffee/component/form/register/register.component';
 import { FooterComponent } from './five-coffee/component/home/footer/footer.component';
@@ -24,6 +27,9 @@ import { FooterComponent } from './five-coffee/component/home/footer/footer.comp
     BrowserModule,
     AppRoutingModule
   ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+],
   providers: [],
   bootstrap: [AppComponent]
 })
